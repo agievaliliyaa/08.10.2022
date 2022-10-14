4c1562937737b09a7b32aed9b31d5d73b2f8985e
agievaliliyaa <agievaliliyaa@gmail.com>
2022-10-15 00:40:20 +0500

    Add project files.

---
+++ 08.10.2022/Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Diagnostics.CodeAnalysis;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public enum SubjectExam_1
+        {
+            english = 1,
+            physics,            //1.Нормально назвать информатику
+            computersciense
+
+        }
+        public struct Students_1
+        {
+            public string firstname_1;
+            public string secondname_1;
+            public int age_1;
+            public SubjectExam_1 SubjectExam;
+            public int points_1;    //2. Нормально назвать переменную
+
+        }
+        public struct Citizens_3
+        {
+            public string name_3;
+            public int id_3;
+            public string problem_3;
+            public int iq_3;
+            public int character_3;
+        }
+        public static void Main(string[] args)
+        {
+            Students_1 Agieva = new Students_1 { firstname_1 = "Агиева", secondname_1 = "Лилия", age_1 = 18, points_1 = 260, SubjectExam = SubjectExam_1.english };
+            Students_1 Arsentev = new Students_1 { firstname_1 = "Арсентьев", secondname_1 = "Кирилл", age_1 = 18, points_1 = 243, SubjectExam = SubjectExam_1.physics };
+            Students_1 Ivanova = new Students_1 { firstname_1 = "Иванова", secondname_1 = "Карина", age_1 = 18, points_1 = 255, SubjectExam = SubjectExam_1.english };
+            Students_1 Makarova = new Students_1 { firstname_1 = "Макарова", secondname_1 = "Дарья", age_1 = 18, points_1 = 267, SubjectExam = SubjectExam_1.computersciense };
+            Students_1 Makhmutova = new Students_1 { firstname_1 = "Махмутова", secondname_1 = "Хадижа", age_1 = 18, points_1 = 261, SubjectExam = SubjectExam_1.english };
+            Students_1 Mitrofanov = new Students_1 { firstname_1 = "Митрофанов", secondname_1 = "Леонид", age_1 = 18, points_1 = 250, SubjectExam = SubjectExam_1.physics };
+            Students_1 Morozov = new Students_1 { firstname_1 = "Морозов", secondname_1 = "Даниил", age_1 = 18, points_1 = 251, SubjectExam = SubjectExam_1.computersciense };
+            Students_1 Saitov = new Students_1 { firstname_1 = "Саитов", secondname_1 = "Марат", age_1 = 18, points_1 = 256, SubjectExam = SubjectExam_1.computersciense };
+            Students_1 Khamitov = new Students_1 { firstname_1 = "Хамитов", secondname_1 = "Ринат", age_1 = 18, points_1 = 234, SubjectExam = SubjectExam_1.computersciense };
+            Students_1 Shaposhnikova = new Students_1 { firstname_1 = "Щапошникова", secondname_1 = "Екатерина", age_1 = 18, points_1 = 270, SubjectExam = SubjectExam_1.computersciense };
+
+            Dictionary<string, Students_1> StudentsDic = new Dictionary<string, Students_1>()
+            {
+                {"Агиева", Agieva },
+                {"Арсентьев", Arsentev},
+                {"Иванова", Ivanova },
+                {"Макарова", Makarova },
+                {"Махмутова", Makhmutova },
+                {"Митрофанов", Mitrofanov },
+                {"Морозов", Morozov },
+                {"Саитов", Saitov },
+                {"Хамитов", Khamitov },
+                {"Шпаошникова", Shaposhnikova }
+            };
+            Console.WriteLine("Домашнее задание #1. Студенты.");
+            Console.WriteLine("Словарь до преобразований.");
+            foreach (var person in StudentsDic)
+            {
+                Console.WriteLine($"Ключ:{person.Key} Значение: {person.Value}");
+            }
+            Console.Write("Введите действие (новый студент, удалить, сортировать): ");
+            string action;
+            action = Console.ReadLine();
+            action.ToLower();
+            switch (action)
+            {
+                case "новый студент":
+                    Console.Write("Введите фамилию студента: ");
+                    string keyname = Console.ReadLine();
+                    Students_1 Name1 = new Students_1();
+                    Console.Write("Введите имя студента: ");
+                    Name1.firstname_1 = Console.ReadLine();
+                    Name1.secondname_1 = keyname;
+                    Console.Write("Введите возраст студента: ");
+                    Name1.age_1 = int.Parse(Console.ReadLine());
+                    Console.Write("Введите сумму баллов за ЕГЭ: ");
+                    Name1.points_1 = int.Parse(Console.ReadLine());
+                    Console.Write("Введите предмет сдачи ЕГЭ (информатика, английский язык, физика): ");
+                    string subject = Console.ReadLine();
+                    subject.ToLower();
+                    switch (subject)
+                    {
+                        case "информатика":
+                            Name1.SubjectExam = SubjectExam_1.computersciense;
+                            break;
+                        case "английский язык":
+                            Name1.SubjectExam = SubjectExam_1.english;
+                            break;
+                        case "физика":
+                            Name1.SubjectExam = SubjectExam_1.physics;
+                            break;
+                        default:
+                            Console.WriteLine("Вы что-то неправильно ввели");
+                            break;
+                    }
+                    StudentsDic.Add(keyname, Name1);
+                    Console.WriteLine("Словарь после преобразований");
+                    foreach (var person in StudentsDic)
+                    {
+                        Console.WriteLine($"Ключ:{person.Key} Значение: {person.Value}");
+                    }
+                    break;
+                case "удалить":
+                    Console.Write("Введите фамилию студента, которого вы хотите удалить: ");
+                    string deletename = Console.ReadLine();
+                    foreach (var person in StudentsDic)
+                    {
+                        if (deletename == person.Key)
+                        {
+                            StudentsDic.Remove(person.Key);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Такого студента не существует");
+                        }
+                    }
+                    Console.WriteLine("Словарь после преобразований.");
+                    foreach (var person in StudentsDic)
+                    {
+                        Console.WriteLine($"Ключ:{person.Key} Значение: {person.Value}");
+                    }
+                    break;
+                case "сортировать": //Сортировка по значению структура?!?!??!?!?!
+                    Console.WriteLine("Словарь после преобразований.");
+                    foreach (var person in StudentsDic)
+                    {
+                        Console.WriteLine($"Ключ:{person.Key} Значение: {person.Value}");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Что-то не так...");
+                    break;
+            }
+
+            Console.WriteLine("Домашнее задание #2. Викинги и пиво.");
+            int[] BBBArray = new int[5];
+            Console.Write("Введите числа, названные игроками Bavarian Beer Bears: ");
+            int counterBBB = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                BBBArray[i] = int.Parse(Console.ReadLine());
+
+
+            }
+            for (int i = 0; i < 5; i++)
+            {
+
+                if (BBBArray[i] == 5)
+                {
+                    counterBBB++;
+                    Console.WriteLine($"Количество 5: {counterBBB}");
+                }
+
+            }
+
+            int[] SSArray = new int[5];
+            Console.Write("Введите числа, названные игроками Scandinavian Schollers: ");
+            int counterSS = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                SSArray[i] = int.Parse(Console.ReadLine());
+
+            }
+            for (int i = 0; i < 5; i++)
+            {
+
+                if (SSArray[i] == 5)
+                {
+                    counterSS++;
+                    Console.WriteLine($"Количество 5: {counterSS}");
+                }
+            }
+            if (counterBBB == counterSS)
+            {
+                Console.WriteLine("Drinks All Round! Free Beers on Bjorg!");
+            }
+            else
+            {
+                Console.WriteLine("Ой, Бьорг - пончик! Ни для кого пива!");
+            }
+
+            Console.WriteLine("Домашнее задание #3. ЖЭК");
+            Citizens_3 Rick = new Citizens_3 { name_3 = "Rick", id_3 = 1, problem_3 = "оплата", iq_3 = 1, character_3 = 10 };
+            Citizens_3 Morty = new Citizens_3 { name_3 = "Morty", id_3 = 2, problem_3 = "отопление", iq_3 = 0, character_3 = 4 };
+            Citizens_3 Deyneris = new Citizens_3 { name_3 = "Deyneris", id_3 = 3, problem_3 = "завоевание мира", iq_3 = 1, character_3 = 6 };
+            Citizens_3 Karchevsky = new Citizens_3 { name_3 = "Karchevsky", id_3 = 4, problem_3 = "провести пару онлайн", iq_3 = 1, character_3 = 2 };
+            Citizens_3 Raskolnikov = new Citizens_3 { name_3 = "Raskolnikov", id_3 = 5, problem_3 = "оплата", iq_3 = 0, character_3 = 1 };
+            Stack<Citizens_3> Zina = new Stack<Citizens_3>(5);
+            Zina.Push(Rick);
+            Zina.Push(Morty);
+            Zina.Push(Deyneris);
+            Zina.Push(Karchevsky);
+            Zina.Push(Raskolnikov);
+            Dictionary<int, Citizens_3> Window1 = new Dictionary<int, Citizens_3>(); //отопление
+            Dictionary<int, Citizens_3> Window2 = new Dictionary<int, Citizens_3>(); //оплата
+            Dictionary<int, Citizens_3> Window3 = new Dictionary<int, Citizens_3>(); //другое
+        
+            if (Zina.Count > 0)
+            {
+                Citizens_3 citizen = Zina.Pop(); // Доделать!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+               
+                switch (citizen.problem_3)
+                {
+                    case "оплата":
+                        Window2.Add(citizen.id_3, citizen);
+                        break;
+                    case "отопление":
+                        Window1.Add(citizen.id_3, citizen);
+                        break;
+                    default:
+                        Window3.Add(citizen.id_3, citizen);
+                        break;
+                }
+
+            }
+
+            Console.WriteLine("Домашнее задание #4. Графы");
+            
+
+        }
+
+        }
+
+
+    }
+
---
+++ ClassWork/Program.cs
@@ -0,0 +1,233 @@
+﻿namespace Toomakov_14._10._22
+{
+    internal class Program
+    {
+
+        static string Сounter(char[] arr)
+        {
+            char[] glas = "aeiouy".ToCharArray();
+            char[] soglas = "bcdfghjklmnpqrstvwxyz".ToCharArray();
+            int countglas = 0;
+            int countsoglas = 0;
+
+            foreach (char ch in arr)
+            {
+                foreach (char g in glas)
+                {
+
+                    if (ch == g)
+                    {
+                        countglas++;
+                    }
+                }
+            }
+            foreach (char ch in arr)
+            {
+                foreach (char g in soglas)
+                {
+
+                    if (ch == g)
+                    {
+                        countsoglas++;
+                    }
+                }
+            }
+            return "Количество гласных: " + Convert.ToString(countglas) + '\n' + "Количество согласных : " + Convert.ToString(countsoglas);
+
+
+        }
+
+
+        static int[,] Mat(int[,] array1, int[,] array2)
+        {
+            try
+            {
+                int len1_0 = array1.GetLength(0);  // число строк 1 матрицы
+                int len1_1 = array1.GetLength(1);
+                int len2_0 = array2.GetLength(0); // число строк
+                int len2_1 = array2.GetLength(1);
+
+                if (len1_1 == len2_0)
+                {
+                    int[,] new_matr = new int[len1_0, len2_1];
+
+                    for (int i = 0; i < len1_0; i++)
+                    {
+                        for (int j = 0; j < len2_1; j++)
+                        {
+                            for (int k = 0; k < len1_1; k++)
+                            {
+                                new_matr[i, j] += array1[i, k] * array2[k, j];
+                            }
+                        }
+                    }
+                    return new_matr;
+                }
+                else
+                {
+                    Console.WriteLine("Количество столбцов первой матрицы не равно количеству строк второй!");
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+
+        }
+        static void PrintMat(int[,] new_matr, int[,] array1, int[,] array2)
+        {
+            for (int y = 0; y < Mat(array1, array2).GetLength(0); y++)
+            {
+                for (int x = 0; x < Mat(array1, array2).GetLength(1); x++)
+                {
+                    Console.Write(Mat(array1, array2)[y, x] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+
+
+        }
+        static int[] Temp(int[,] arr1)
+        {
+            int[] ints = new int[arr1.GetLength(0)];
+            for (int i = 0; i < arr1.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr1.GetLength(1) - 1; j++)
+                {
+                    ints[i] += arr1[i, j] + arr1[i, j + 1];
+                }
+
+            }
+            return ints;
+        }
+
+        static string Simbols(List<char> list)
+        {
+            List<char> glas = new List<char>();
+            string str_1 = ("aeiouy");
+            foreach (char a in str_1)
+            {
+                glas.Add(a);
+            }
+            List<char> soglas = new List<char>();
+            string str_2 = ("bcdfghjklmnpqrstvwxyz");
+            foreach (char b in str_2)
+            {
+                soglas.Add(b);
+            }
+            int countglas = 0;
+            int countsoglas = 0;
+
+            foreach (char ch in list)
+            {
+                foreach (char g in glas)
+                {
+
+                    if (ch == g)
+                    {
+                        countglas++;
+                    }
+                }
+            }
+            foreach (char ch in list)
+            {
+                foreach (char g in soglas)
+                {
+
+                    if (ch == g)
+                    {
+                        countsoglas++;
+                    }
+                }
+            }
+            return "Количество гласных: " + Convert.ToString(countglas) + '\n' + "Количество согласных : " + Convert.ToString(countsoglas);
+
+
+        }
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("ДЗ.Задача 6.1");
+            string str = "c:users\\admin\\github\\toomakov";
+            char[] arr = str.ToCharArray();
+
+            Console.WriteLine(Сounter(arr));
+
+
+            Console.WriteLine("ДЗ.Задача 6.2");
+            int[,] array1 = new int[,]
+            {
+                {1, 2, 3},
+                {5, 6, 8},
+
+            };
+            int[,] array2 = new int[,]
+            {
+                {9, 2},
+                {4, 7},
+                {8, 4}
+
+            };
+            PrintMat(Mat(array1, array2), array1, array2);
+
+
+
+            Console.WriteLine("ДЗ.Задача 6.3");
+
+            int[,] arr1 = new int[12, 30];
+            Random rand = new Random();
+
+            for (int y = 0; y < arr1.GetLength(0); y++)
+            {
+                for (int x = 0; x < arr1.GetLength(1); x++)
+                {
+                    arr1[y, x] = rand.Next(-30, 30);
+
+                }
+            }
+            for (int i = 0; i < Temp(arr1).Length; i++)
+            {
+                Console.WriteLine(Temp(arr1)[i]);
+            }
+
+
+
+            Console.WriteLine("ДЗ.Задача 6.1.1");
+            List<char> list = new List<char>();
+            string str1 = ("c:users\\admin\\github\\toomakov");
+            foreach (char c in str1)
+            {
+                list.Add(c);
+            }
+            Console.WriteLine(Simbols(list));
+
+
+
+
+            Console.WriteLine("ДЗ.Задача 6.1.2");
+            List<int> list1 = new List<int>() { 1, 2, 3 };
+            List<int> list2 = new List<int>() { 5, 6, 8 };
+
+            List<List<int>> listOfLists = new List<List<int>>();
+            listOfLists.Add(list1);
+            listOfLists.Add(list2);
+
+            List<int> list3 = new List<int>() { 9, 2, };
+            List<int> list4 = new List<int>() { 4, 7, };
+            List<int> list5 = new List<int>() { 8, 4, };
+
+            List<List<int>> listOfLists1 = new List<List<int>>();
+            listOfLists.Add(list3);
+            listOfLists.Add(list4);
+            listOfLists.Add(list5);
